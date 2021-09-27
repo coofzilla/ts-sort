29461223f49be3a9be12af9081a256393645439f
Jay Hernandez <jeric614@yahoo.com>
2021-09-27 13:52:23 +0900

    implemented sorting f/arbitrary collections

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@
 //config concurrently script in package.json
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([120, 31, -32, 0,]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([120, 31, -32, 0,]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
+
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
